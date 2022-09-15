07e8782391f14ffca54d2a7d20054d108268fd41
jvhe <jeongbae1212@gmail.com>
2022-09-15 18:51:28 +0900

    feat: InputView 구현

---
+++ src/main/kotlin/RacingCar/UI/InputView.kt
@@ -0,0 +1,15 @@
+package RacingCar.UI
+
+class InputView {
+
+    fun inputNumberOfCars(): String {
+        println("자동차 대수는 몇 대 인가요?")
+        return readLine()!!
+    }
+
+    fun inputNumberOfAttempts(): Int {
+        println("시도할 회수는 몇 회인가요?")
+        return readLine()!!.toInt()
+    }
+
+}
